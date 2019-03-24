dd1c1c5fa9068b6cc8e4a64ee5e7950633541d16
Definition of Dean <defofdean@gmail.com>
2019-03-24 14:58:45 +0000

    Add C++2a section

--- main.cpp
+++ main.cpp
@@ -188,4 +188,8 @@ auto main() -> int {
 
   // Algorithms
   // std::iota
+
+  // C++2a (what will become C++20)
+  // three-way comparison using the "spaceship operator", operator <=>
+  // designated initializers
 }
